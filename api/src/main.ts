25bd1d2880ec6118ff36be1fc4b625a26f26ce31
trevisharp <leonardotrevisansilio@gmail.com>
2026-05-29 12:17:42 -0300

    init docker, api and ui empty project

---
+++ api/src/main.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (req, res) => res.status(200).end());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running on ${port}`);
+});

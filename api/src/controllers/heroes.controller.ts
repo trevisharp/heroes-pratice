00f0c67312a3086817c6a309e62e48281d1b6aea
trevisharp <leonardotrevisansilio@gmail.com>
2026-05-29 13:09:15 -0300

    create empty routes for heroes

---
+++ api/src/controllers/heroes.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+
+export async function getHeroes(req: Request, res: Response) {
+    res.status(501).end()
+}
+
+export async function getHero(req: Request, res: Response) {
+    res.status(501).end()
+}
+
+export async function createHero(req: Request, res: Response) {
+    res.status(501).end()
+}
+
+export async function editHero(req: Request, res: Response) {
+    res.status(501).end()
+}
+
+export async function deleteHero(req: Request, res: Response) {
+    res.status(501).end()
+}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { heroesRouter } from "./routes/heroes.router";
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/health", (req, res) => res.status(200).end());
+
+app.use("/heroes", heroesRouter)
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
